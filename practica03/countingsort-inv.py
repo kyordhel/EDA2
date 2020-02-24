253e794901d84ff24099a82c9cc2ed3669743c50
Mauricio Matamoros <kyordhel@gmail.com>
2020-02-24 15:52:37 -0600

    Add inverse countingsort

---
+++ practica03/countingsort-inv.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Future imports (Python 2.7 compatibility)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import sys
+
+def countingSort(A, k):
+	count=[ 0 for i in range(0, k+1) ]
+	output=[ 0 for i in range(0, len(A)) ]
+
+	for i in range(0, len(A)):
+		count[A[i]]+= 1
+	print(count)
+
+	# for i in range(1, k+1):
+	# 	count[i]+= count[i-1]
+	# print(count)
+
+	# for i in range(len(A)-1, -1, -1):
+	# 	output[count[A[i]]-1] = A[i]
+	# 	count[A[i]]-=1
+
+	j = 0
+	for i in range(len(count)-1, -1, -1):
+		while count[i] > 0:
+			output[j] = i
+			count[i]-=1
+			j+=1
+
+	return output
+
+def main():
+	A=[	8, 7, 3, 6, 9, 8, 5, 8, 4, 6,
+		9, 8, 7, 5, 9, 6, 1, 4, 8, 4,
+		3, 2, 7, 9, 9, 8, 7, 5, 3, 9,
+		9, 5, 7, 6, 5, 6, 7, 8, 9, 9,
+		6, 2, 4, 9
+	]
+	A = countingSort(A,9)
+	print(A)
+
+
+if __name__ == '__main__':
+	main()
